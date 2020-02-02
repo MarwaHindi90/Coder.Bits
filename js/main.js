f18676b772ae4dce9fcf5b8c1c438f593f5dfdbc
Marwa Hindi <marwashindi@gmail.com>
2020-02-02 11:13:09 +0200

    coderBits

---
+++ js/main.js
@@ -0,0 +1,99 @@
+
+
+// Team Carousel
+$('.testimonial-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+});
+
+//// Process Carousel
+//$('.process-carousel').owlCarousel({
+//    loop:true,
+//    nav:true,
+//    dots:false,
+//    autoplay:true,
+//    autoplayTimeout:2000,
+//    autoplayHoverPause:true,
+//    responsiveClass:true,
+//    navText: 
+//    ["<span class='carousel-nav-right'><i class='fas fa-long-arrow-alt-left'></i></span>",
+//     
+//    "<span class='carousel-nav-left'><i class='fas fa-long-arrow-alt-right'></i></span>"],
+//    responsive:{
+//        0:{
+//            items:1,
+//        },
+//        600:{
+//            items:1,
+//        },
+//        1000:{
+//            items:1,
+//        }
+//    }
+//});
+
+
+
+// Partner Carousel
+//$('.partner-carousel').owlCarousel({
+//    loop:true,
+//    nav:false,
+//    dots:false,
+//    autoplay:true,
+//    autoplayTimeout:3000,
+//    autoplayHoverPause:true,
+//    responsive:{
+//        0:{
+//            items:1
+//        },
+//        600:{
+//            items:3
+//        },
+//        1000:{
+//            items:5
+//        }
+//    }
+//});
+
+
+//// testimonial Carousel
+//$('.testimonial-carousel').owlCarousel({
+//    loop:true,
+//    nav:false,
+//    margin:10,
+//    dots:true,
+//    autoplay:false,
+//    responsive:{
+//        0:{
+//            items:1
+//        },
+//        600:{
+//            items:1
+//        },
+//        1000:{
+//            items:1
+//        }
+//    }
+//});
+   
+// Counter 
+//$('.counter').counterUp({
+//    delay: 10,
+//    time: 1000
+//});
+
+
+
+
